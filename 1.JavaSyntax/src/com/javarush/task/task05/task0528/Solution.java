a35d49d75f06e702868590712ff65ab742fb6dba
bobrovitskiy_a <shmoxandco@mail.ru>
2017-06-08 13:42:54 +0300

    1

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0528/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task05.task0528;
+
+/* 
+Вывести на экран сегодняшнюю дату
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("08 06 2017");//напишите тут ваш код
+    }
+}
