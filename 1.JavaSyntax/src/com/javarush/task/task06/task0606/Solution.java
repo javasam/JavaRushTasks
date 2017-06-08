167375155ac272c98e0ef45ba72a8be962b3714b
bobrovitskiy_a <shmoxandco@mail.ru>
2017-06-08 16:44:56 +0300

    1

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0606/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task06.task0606;
+
+import java.io.*;
+
+/* 
+Чётные и нечётные циферки
+*/
+
+public class Solution {
+
+    public static int even;
+    public static int odd;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        for (int i = 0; i < s.length(); i++) {
+            int a = Integer.parseInt(s.substring(i, i + 1));
+            if (a % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+        System.out.printf("Even: %d Odd: %d", even, odd);
+    }
+}
