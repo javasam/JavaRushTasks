53ddf6c01b0baaef5ca7c8591de5a7fc11679602
bobrovitskiy_a <shmoxandco@mail.ru>
2017-06-08 13:55:52 +0300

    1

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0529/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task05.task0529;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Консоль-копилка
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+        String s;
+
+        while (!"сумма".equals(s = br.readLine()) ) {
+            int a = Integer.parseInt(s);
+            sum += a;
+        }
+        System.out.println(sum);
+    }//напишите тут ваш код
+    }
+
