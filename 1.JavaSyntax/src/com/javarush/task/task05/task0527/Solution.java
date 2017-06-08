abf00348503744153dfdbf06d806d2398587967b
bobrovitskiy_a <shmoxandco@mail.ru>
2017-06-08 13:36:09 +0300

    1

--- 1.JavaSyntax/src/com/javarush/task/task05/task0527/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0527/Solution.java
@@ -7,6 +7,8 @@ package com.javarush.task.task05.task0527;
 public class Solution {
     public static void main(String[] args) {
         Mouse jerryMouse = new Mouse("Jerry", 12, 5);
+        Cat tomcat = new Cat ("Tom", 3, 5);
+        Dog boyDog = new Dog ("Boy", 3, 4);
 
         //напишите тут ваш код
     }
@@ -23,5 +25,27 @@ public class Solution {
         }
     }
 
-    //напишите тут ваш код
+    public static class Cat {
+        String name;
+        int height;
+        int weight;
+
+        public Cat (String name, int height, int weight){
+            this.name = name;
+            this.height = height;
+            this.weight = weight;
+        }
+    }
+
+    public static class Dog {
+        String name;
+        int age;
+        int weight;
+
+        public Dog (String name, int age, int weight) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+        }
+    }
 }
